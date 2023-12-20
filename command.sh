a8d87b8eb998084c35aefd949a7e045921de52c5
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-12-20 13:10:01 +0200

    Command

--- command.sh
+++ command.sh
@@ -1,24 +1,14 @@
-# REPO_URL https://github.com/manueldinisjunior/my-app
-# Commands used in steps 1-8 
-
-# Create react app 
-npx create-react-app blackbird-task-1
-cd blackbird-task-1
-
-# Log into GitHub CLI 
+npx create-react-app base-react-app
+cd base-react-app
 gh auth login
-
-# Create repo and checkout to the "update_logo" branch
 gh repo create
 git checkout -b update_logo
-
-# After making changes, commit and push them up
 sed -i "" 's|img src={logo}|img src="https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg"|g' src/App.js
 sed -i "" 's|href="https://reactjs.org"|href="https://www.propelleraero.com/dirtmate/"|g' src/App.
 git add .
-git commit -m "Update logo to Propeller and link to Dirtmate"
-git push --set-upstream origin update_logo
-
-# Create and then approve a PR
-gh pr create --title "Update logo and link"
+git commit -m "change logo and link"
+git push origin update_logo
+gh pr create --title "Update Logo" --body "Updating logo and link"
 gh pr merge
+
+# REPO_URL: https://github.com/manueldinisjunior/blackbird-task-1
